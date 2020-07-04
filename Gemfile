3d968869b3203cd2992468fc49c1ff8fc06664fe
Renan Proenca <renan.maringolo@gmail.com>
2020-07-04 20:06:48 -0300

    [WIP] Write test two scenario

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'bootsnap', '>= 1.4.2', require: false
 gem 'devise'
 
 group :development, :test do
+  gem 'capybara'
   gem 'rspec-rails'
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 end
---
+++ spec/features/register_user_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+feature 'register user' do
+  scenario 'succesfully' do
+    visit root_path
+    click_on 'Sign up'
+    fill_in 'Email', with: 'renan@gmail.com'
+    fill_in 'Password', with: '12345678'
+    fill_in 'Password confirmation', with: '12345678'
+    click_on 'Sign up'
+
+    expect(page).to_not have_link('Sign up')
+  end
+
+  scenario 'sign in' do
+    user = User.create(email: 'renan@renan.com', encrypted_password: '1234567890')
+
+    visit root_path
+    click_on 'Sign in'
+    fill_in 'Email', with: 'renan@gmail.com'
+    fill_in 'Password', with: '12345678'
+    click_on 'Log in'
+
+    expect(page).to_not have_link('Sign in')
+  end
+end
